e1ec36ea85c4edcd51287122adcf42be2b6f24d2
csy1204 <josang1204@gmail.com>
2017-04-04 08:12:46 +0000

    first commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,21 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+
+  end
+
+  def create
+    post = Post.new(post_params)
+    post.save
+
+    redirect_to posts_index_path
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
+end
